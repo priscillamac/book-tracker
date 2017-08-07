36627e850aee1f0c7e511f268f02d043b17e7626
Priscilla Macalincag <priscilla@optimalam.com>
2017-08-07 13:57:39 +1200

    type:refactor renamed file names and split books and search into their own components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import App from './app'
+import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>, 
+  document.getElementById('root')
+)
